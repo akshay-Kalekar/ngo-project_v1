ae18d24076ec6b1ab3e6987ede4b4a664471b3a7
Akshay <akalekar2003@gmail.com>
2023-02-20 19:55:44 +0530

    trying on image

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Card } from "./Card";
 import { Details } from "./Details";
 import { motion } from "framer-motion"
+import Cover from '../../assets/gallery/elements/Cover.png'
 export const Gallery = () => {
-
-
+  
+// import {pic1.png } from '../../assets/gallery/display/'
   return (
     //Red Nav
 
     <div className="flex flex-col h-fit md:h-[100vh] text-white bg-white ">
-    <div className="bg-[url('gallery/elements/Cover.jpg')] bg-cover bg-center h-[10vh]  w-[100vw] flex items-center  justify-between    ">
+    <div className={`bg-[url(${Cover})] bg-cover bg-center h-[10vh]  w-[100vw] flex items-center  justify-between    `}>
     <div></div>
     <div className=" font-semibold text-2xl  pl-[15%]">GALLERY</div>
         <div className="pr-[10%] cursor-pointer">View All &gt; </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pic1': "url('./src/assets/gallery/display/pic1.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
